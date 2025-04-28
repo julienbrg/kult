16fde4932f1e19c6f508469da4053c5e456f6100
julienbrg <julien@strat.cc>
2025-04-28 12:47:25 +0200

    add settings page

---
+++ src/app/settings/layout.tsx
@@ -0,0 +1,34 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Settings | Kult',
+  description: 'Unleash your imagination in this new page!',
+
+  openGraph: {
+    title: 'Settings | Kult',
+    description: 'Unleash your imagination in this new page!',
+    url: 'https://kult-app.netlify.app/settings',
+    siteName: 'Kult',
+    images: [
+      {
+        url: '/huangshan.png',
+        width: 1200,
+        height: 630,
+        alt: 'Store the books, movies and artworks you loved!',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: 'New Page | Kult',
+    description: 'Unleash your imagination in this new page!',
+    images: ['/huangshan.png'],
+  },
+}
+
+export default function NewLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>
+}
